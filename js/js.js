b0cc8e523153fe1acbc12eec01a46430a847fec9
Santiago Palmieri <santiago.palmieri97@gmail.com>
2023-06-26 12:50:37 -0300

    Cuatro Esquinitas

---
+++ js/js.js
@@ -0,0 +1,17 @@
+function openModal(modalId) {
+    var modal = document.getElementById(modalId + 'Modal');
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId + 'Modal');
+    modal.style.display = "none";
+}
+
+function iniciarSesion() {
+    alert('Iniciando sesion...');
+}
+
+function registrarse() {
+    alert('Registrandose...');
+}
